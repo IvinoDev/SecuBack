1bebf315876c2168900ed986caf1952927b7a425
IvinoDev <kayantao@protonmail.com>
2022-11-22 15:47:32 +0000

    initial commit

---
+++ src/main/java/ml/secucom/secuback/SecuBackApplication.java
@@ -0,0 +1,13 @@
+package ml.secucom.secuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ml/secucom/secuback/SecuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.secucom.secuback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
